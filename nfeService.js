e4f8ccde329e9572db40d4dcddad7cd849342336
andressa1998 <andmiotto1998@gmail.com>
2026-06-15 12:45:23 -0300

    23

--- nfeService.js
+++ nfeService.js
@@ -15,6 +15,7 @@ class NFEService {
         }
     }
 
+    // Remove espaços, quebras de linha e declaração XML
     _cleanXml(xml) {
         return xml
             .replace(/<\?xml.*?\?>/g, '')
@@ -25,6 +26,12 @@ class NFEService {
     }
 
     async sendNFe(xmlAssinado, certData) {
+        // Limpa o XML (remove quebras de linha, tabs, espaços entre tags)
+        let xmlLimpo = this._cleanXml(xmlAssinado);
+
+        // Constrói o envelope SOAP sem espaços extras
+        const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeAutorizacao4">${xmlLimpo}</nfeDadosMsg></soap:Body></soap:Envelope>`;
+
         const httpsAgent = new https.Agent({
             cert: certData.cert,
             key: certData.privateKey,
@@ -33,18 +40,6 @@ class NFEService {
             secureProtocol: 'TLSv1_2_method',
             ciphers: 'DEFAULT@SECLEVEL=1'
         });
-
-        let xmlLimpo = this._cleanXml(xmlAssinado);
-        const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-               xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-    <soap:Body>
-        <nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeAutorizacao4">
-            ${xmlLimpo}
-        </nfeDadosMsg>
-    </soap:Body>
-</soap:Envelope>`;
 
         const response = await axios.post(this.urlAutorizacao, soapEnvelope, {
             httpsAgent,
@@ -58,6 +53,8 @@ class NFEService {
     }
 
     async sendEvento(xmlAssinado, certData) {
+        let xmlLimpo = this._cleanXml(xmlAssinado);
+        const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4">${xmlLimpo}</nfeDadosMsg></soap:Body></soap:Envelope>`;
         const httpsAgent = new https.Agent({
             cert: certData.cert,
             key: certData.privateKey,
@@ -66,17 +63,6 @@ class NFEService {
             secureProtocol: 'TLSv1_2_method',
             ciphers: 'DEFAULT@SECLEVEL=1'
         });
-        let xmlLimpo = this._cleanXml(xmlAssinado);
-        const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-               xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-    <soap:Body>
-        <nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4">
-            ${xmlLimpo}
-        </nfeDadosMsg>
-    </soap:Body>
-</soap:Envelope>`;
         const response = await axios.post(this.urlEvento, soapEnvelope, {
             httpsAgent,
             headers: {
@@ -89,6 +75,7 @@ class NFEService {
     }
 
     async consultarStatus(chaveAcesso, certData) {
+        const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeConsultaProtocolo4"><consSitNFe xmlns="http://www.portalfiscal.inf.br/nfe" versao="4.00"><tpAmb>${this.ambiente === 'producao' ? '1' : '2'}</tpAmb><xServ>CONSULTAR</xServ><chNFe>${chaveAcesso}</chNFe></consSitNFe></nfeDadosMsg></soap:Body></soap:Envelope>`;
         const httpsAgent = new https.Agent({
             cert: certData.cert,
             key: certData.privateKey,
@@ -97,18 +84,6 @@ class NFEService {
             secureProtocol: 'TLSv1_2_method',
             ciphers: 'DEFAULT@SECLEVEL=1'
         });
-        const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-    <soap:Body>
-        <nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeConsultaProtocolo4">
-            <consSitNFe xmlns="http://www.portalfiscal.inf.br/nfe" versao="4.00">
-                <tpAmb>${this.ambiente === 'producao' ? '1' : '2'}</tpAmb>
-                <xServ>CONSULTAR</xServ>
-                <chNFe>${chaveAcesso}</chNFe>
-            </consSitNFe>
-        </nfeDadosMsg>
-    </soap:Body>
-</soap:Envelope>`;
         const response = await axios.post(this.urlConsulta, soapEnvelope, {
             httpsAgent,
             headers: { 'Content-Type': 'text/xml; charset=utf-8' },
